8b8dbbd802ec2a2cd9415cfd425774ebc032196f
abhi <50333572+abhionly009@users.noreply.github.com>
2026-05-21 21:53:38 +0530

    add code for association example

---
+++ src/aaaaaaaaaaaaaaaaaaaaaaaaCopy/Engine.java
@@ -0,0 +1,14 @@
+package aaaaaaaaaaaaaaaaaaaaaaaaCopy;
+
+public class Engine {
+
+    String type;
+
+    public Engine(String type){
+        this.type =type;
+    }
+
+    public void showEngine(){
+        System.out.println("Engine type :"+type);
+    }
+}
