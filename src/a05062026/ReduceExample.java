20741123a0df87bcb33ee83f63b3f78441c75d46
abhi <50333572+abhionly009@users.noreply.github.com>
2026-06-10 11:56:48 +0530

    practiced some more java code for reduce function and flatmap

--- src/a05062026/ReduceExample.java
+++ src/a05062026/ReduceExample.java
@@ -1,6 +1,8 @@
 package a05062026;
 
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class ReduceExample {
 
@@ -31,11 +33,52 @@ public class ReduceExample {
         List<String> words =
                 List.of("Java", "Spring", "Kafka");
 
-     String sentence =   words.stream().reduce("", (a,b)->a+ " " +b);
+        String sentence =   words.stream().reduce("", (a,b)->a+ " " +b);
 
         System.out.println(sentence);
 
 
+
+
+
+
+
+        Integer maxNumber = numbers.stream().reduce(Integer.MIN_VALUE,Integer::max);
+        System.out.println(maxNumber);
+
+
+        String str = "Hello world";
+
+        // count number of vowel in given String using stream api
+
+        long count = str.chars().mapToObj(c -> (char) c)
+                .filter(ch -> "aieou".indexOf(ch) == -1)
+                .count();
+
+        System.out.println(count);
+
+
+        String input = "Hi I am abhinandan shah";
+
+        input.replace(" ","").chars().mapToObj(c->(char)c)
+                .filter(ch->"aeiouAEIOU".indexOf(ch)!=-1)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().forEach(System.out::println);
+
+
+
+        input.chars().mapToObj(c->(char)c)
+                .collect(Collectors.partitioningBy(ch->"aeiouAEIOU".indexOf(ch)!=-1))
+                .entrySet().stream().forEach(System.out::println);
+
+
+
+
+        String va = "andjin23d3e2";
+
+        va.chars().mapToObj(c->(char)c)
+                .filter(ch->Character.isLetter(ch))
+                .forEach(System.out::print);
 
 
     }
